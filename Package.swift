fae0c28074459fb4ceb405a293c879a4e9f6ec88
Simone Civetta <viteinfinite@gmail.com>
2016-03-07 18:07:59 +0100

    [dependencies] Add Kitura

--- Package.swift
+++ Package.swift
@@ -1,26 +1,9 @@
-/**
- * Copyright IBM Corporation 2016
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
+import PackageDescription
 
 import PackageDescription
 
 let package = Package(
-  name: "HelloWorldSwift",
-  targets: [
-    Target(name: "Utils", dependencies: []),
-    Target(name: "Server", dependencies: [.Target(name: "Utils")])
-  ],
-  dependencies: []
-)
+    name: "XebiaBattle",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,3,0)..<Version(0,4,0)),
+])
